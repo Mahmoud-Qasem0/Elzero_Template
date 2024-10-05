69d4c31c491e43e1ae17889fefbda84eb77f321a
Mahmoud_Qasem0 <mahmoudqasem1@hotmail.com>
2024-10-05 07:28:58 +0300

    Uploaded design files

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// Date updating in Footer
+let year = document.getElementById("year");
+
+year.innerHTML = new Date().getFullYear();
+
+// Scroll To Top Button
+
+function scrollToTop() {
+  window.scrollTo(0, 0);
+}
